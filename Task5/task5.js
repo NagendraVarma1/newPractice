502b93d579b0fe3bd9b8db977af78d3652da59bf
NagendraVarma1 <nagendravarmal512@gmail.com>
2023-02-27 17:27:08 +0530

    Task 5

---
+++ Task5/task5.js
@@ -0,0 +1,15 @@
+//GETELEMENTSBYCLASSNAME
+
+let items=document.getElementsByClassName('list-group-item');
+
+for(let i=0;i<items.length;i++){
+    items[i].style.backgroundColor = '#f4f4f4';
+}
+
+//GETELEMENTSBYTAGNAME
+
+let li = document.getElementsByTagName('li')
+
+for(let i=0; i<li.length; i++){
+    li[i].style.backgroundColor = '#f4f4f4'
+}
